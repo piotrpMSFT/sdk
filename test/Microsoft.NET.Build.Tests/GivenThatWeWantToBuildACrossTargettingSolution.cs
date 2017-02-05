4232ea5ec42a5614e48ae9067050e87b9498ed54
Piotr Puszkiewicz <piotrp@microsoft.com>
2017-02-04 18:59:59 -0800

    Build works for new-style projects

--- test/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildACrossTargettingSolution.cs
+++ test/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildACrossTargettingSolution.cs
@@ -3,6 +3,7 @@
 
 using System.IO;
 using System.Runtime.InteropServices;
+using Microsoft.DotNet.InternalAbstractions;
 using Microsoft.NET.TestFramework;
 using Microsoft.NET.TestFramework.Assertions;
 using Microsoft.NET.TestFramework.Commands;
@@ -31,9 +32,22 @@ namespace Microsoft.NET.Build.Tests
                 .Execute("/p:TargetFramework=netcoreapp1.0")
                 .Should()
                 .Pass();
+
+            new DirectoryInfo(Path.Combine(buildCommand.ProjectRootPath, "NetCoreApp", "bin", "Debug", "netcoreapp1.0"))
+                .Should()
+                .OnlyHaveFiles(new[] {
+                    "DesktopAndNetStandard.dll",
+                    "DesktopAndNetStandard.pdb",
+                    "NetCoreApp.deps.json",
+                    "NetCoreApp.dll",
+                    "NetCoreApp.pdb",
+                    "NetCoreApp.runtimeconfig.dev.json",
+                    "NetCoreApp.runtimeconfig.json"
+                });
         }
+
         [Fact]
-        public void It_builds_successfully_for_netstandard15_and_produces_output()
+        public void It_builds_output_for_netstandard15()
         {
             var testAsset = _testAssetsManager
                 .CopyTestAsset("CrossTargettingSolution")
@@ -48,6 +62,35 @@ namespace Microsoft.NET.Build.Tests
             var buildCommand = new BuildCommand(Stage0MSBuild, testAsset.TestRoot, "CrossTargettingSolution.sln");
             buildCommand
                 .Execute("/p:TargetFramework=netstandard1.5")
+                .Should()
+                .Pass();
+
+            new DirectoryInfo(Path.Combine(buildCommand.ProjectRootPath, "DesktopAndNetStandard", "bin", "Debug", "netstandard1.5"))
+                .Should()
+                .OnlyHaveFiles(new[] {
+                    "DesktopAndNetStandard.deps.json",
+                    "DesktopAndNetStandard.dll",
+                    "DesktopAndNetStandard.pdb"
+                });
+        }
+        [Fact]
+        public void It_builds_output_for_the_current_RID()
+        {
+            var rid = RuntimeEnvironment.GetRuntimeIdentifier();
+            
+            var testAsset = _testAssetsManager
+                .CopyTestAsset("CrossTargettingSolution")
+                .WithSource();
+
+            var restoreCommand = new RestoreCommand(Stage0MSBuild, testAsset.TestRoot, "CrossTargettingSolution.sln");
+            restoreCommand
+                .Execute($"/p:RuntimeIdentifier={rid}")
+                .Should()
+                .Pass();
+
+            var buildCommand = new BuildCommand(Stage0MSBuild, testAsset.TestRoot, "CrossTargettingSolution.sln");
+            buildCommand
+                .Execute("/p:TargetFramework=netcoreapp1.0", $"/p:RuntimeIdentifier={rid}")
                 .Should()
                 .Pass();
 
